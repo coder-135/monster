79e67d1a757d79d1f77dc8a2833ac628996331a8
Seyyed Reza <darkness4ever4@gmail.com>
2020-06-03 11:21:28 +0430

    My first React App

---
+++ build/precache-manifest.4f347713d748dbcf4188d0a2023afaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca81f68660484f52b9ab28e2db0290c2",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "e9e8428c750b331719b3",
+    "url": "/monster/static/css/main.8a0ffde0.chunk.css"
+  },
+  {
+    "revision": "ddb1825fe377645e2a12",
+    "url": "/monster/static/js/2.8e3da586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.8e3da586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e8428c750b331719b3",
+    "url": "/monster/static/js/main.476a0e79.chunk.js"
+  },
+  {
+    "revision": "30dcebd3ad4d77823ae4",
+    "url": "/monster/static/js/runtime-main.e9176a79.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import {CardList} from "./components/card-component-folder/cardList";
+
+import {SearchBox} from './components/search-box/search-box-component';
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state={
+            monsters:[],
+            searchField:''}
+
+            // this.handleChange=this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({monsters:users}));
+    }
+
+     handleChange= e=>{
+        this.setState({searchField:e.target.value})
+    }
+
+    render() {
+        const {monsters, searchField}=this.state;
+        const filteredMonsters=monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+        return (
+        <div>
+<h1>Fake Monsters</h1>
+            <SearchBox
+            placeholder={"Search monsters"}
+            handleChange={this.handleChange}
+            />
+
+
+            <CardList monsters={filteredMonsters} />
+
+
+
+        </div>
+
+        )
+    }
+}
+export default App;
+
+
+
